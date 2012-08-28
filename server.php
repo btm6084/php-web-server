f5f73e59e51f67e28890706d5dd159665d54928d
btm6084 <Talindras@gmail.com>
2012-08-28 10:27:56 -0400

    Uncommit changes to \r\n -> PHP_ROL. HTTP requires the \r\n.

--- server.php
+++ server.php
@@ -243,7 +243,7 @@ if (!$socket) {
             print_r($headers);
             echo "\n\n";
 
-            stream_socket_sendto($conn, implode(PHP_EOL, $headers) . PHP_EOL . PHP_EOL);
+            stream_socket_sendto($conn, implode("\r\n", $headers) . "\r\n\r\n");
             stream_socket_sendto($conn, $content);
             fclose($conn);
         }
